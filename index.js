0d3b3adc7c824f80f9c2f2a829fa7a3e7fe9e443
Srija S <srijakumar7@gmail.com>
2018-01-21 17:10:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+leastFavoriteCustomer = "test";
+
+function upperCaseCustomerName () {
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  bestCustomer = "not bob";
+}
