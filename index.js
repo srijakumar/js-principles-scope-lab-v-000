dada7d9b3cbafcff2a8dc405c2cef9c774bb2c5d
Srija S <srijakumar7@gmail.com>
2018-01-21 17:39:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution in this file!
 
 var customerName = "bob";
-leastFavoriteCustomer = "test";
+const leastFavoriteCustomer = "test";
 
 function upperCaseCustomerName () {
   return customerName = customerName.toUpperCase();
